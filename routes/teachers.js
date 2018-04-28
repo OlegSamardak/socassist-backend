5badcc917f5000f34419461cd3c4c36885db96d9
unknown <os1998os@ukr.net>
2018-04-28 11:53:09 +0300

    setted environment consts

--- routes/teachers.js
+++ routes/teachers.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json('users')
+  res.json('teachers')
 });
 
 module.exports = router;
---
+++ services/teacher-service.js
@@ -0,0 +1,2 @@
+const mongo = require('mongodb');
+var url = "mongodb://localhost:27017/socassist";
