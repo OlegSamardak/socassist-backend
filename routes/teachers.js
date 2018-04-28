f12dda30be020a1a749cc398d5af3ecf1c7ec71d
unknown <os1998os@ukr.net>
2018-04-28 13:23:26 +0300

    get teacher from db using promise

--- routes/teachers.js
+++ routes/teachers.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+const findAllTeachers = require('../services/teacher-service');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json('teachers')
+router.get('/', (req, res, next) => {
+  // console.log(findAllTeachers.findAllTeachers());
+  findAllTeachers.findAllTeachers().then(result =>{
+    res.json(result);
+  })
 });
 
 module.exports = router;
--- services/teacher-service.js
+++ services/teacher-service.js
@@ -1,2 +1,22 @@
-const mongo = require('mongodb');
-var url = "mongodb://localhost:27017/socassist";
+const MongoClient = require('mongodb').MongoClient;
+const ENV_CONST = require('../environment.const');
+
+
+findAllTeachers = () =>{
+    return new Promise((resolve, reject) =>
+    {
+        MongoClient.connect(ENV_CONST.DB_URL, (err, db) => {
+            if (err) throw err;
+            let dbo = db.db("socassist");
+            dbo.collection("teachers").find().toArray((err, result) => {
+                if (err) throw err;
+                console.dir(result);
+                db.close();
+                resolve(result);
+            });
+        });
+    })
+};
+module.exports.findAllTeachers = findAllTeachers;
+
+
